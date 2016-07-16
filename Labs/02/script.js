0d787831015c7fa84872739d5a3431b27a3fe3e3
Amir Omidi <amir@aaomidi.com>
2016-07-16 00:51:21 -0400

    Initial test of the project.

--- Labs/02/script.js
+++ Labs/02/script.js
@@ -1,3 +1,26 @@
+var apiKey = "";
 function callAPI() {
+    var element = document.getElementById("input");
+    apiKey = element.value;
+    apiKey = encodeURIComponent(apiKey);
+    var url = "http://api.wunderground.com/api/" + apiKey + "/geolookup/q/autoip.json";
+    $.getJSON(url, {}, getZip);
+}
+
+function getZip(resp) {
+    var zipCode = resp.location.zipcode;
+    if (zipCode == null) {
+        error("Zipcode was not found.");
+        return;
+    }
+    zipCode = encodeURIComponent(zipCode);
+    var url = "http://api.wunderground.com/api/" + apiKey + "/hourly/q/" + zipCode + ".json";
+
+    $.getJSON(url, {}, getLocation());
+}
+function getLocation(resp) {
+    alert(resp);
+}
+function error(error) {
 
 }
