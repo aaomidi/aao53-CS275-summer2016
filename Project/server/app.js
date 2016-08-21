3fabc70e13a4b891894e55782f31bf6120f143a9
Amir Omidi <amir@aaomidi.com>
2016-08-21 02:23:29 -0400

    More logging

--- Project/server/app.js
+++ Project/server/app.js
@@ -100,7 +100,8 @@ app.post('/api/get', function (req, res) {
                     sendResults(result, req, res);
                     return;
                 }
-                console.log("Looping?" + resp.json.results[0].address_compoents.size());
+                console.log(resp.json.results[0]);
+                console.log("\n\nLooping?" + resp.json.results[0].address_compoents.size());
                 for (var i in resp.json.results[0].address_components) {
                     var addr = resp.json.results[0].address_components[i];
                     console.log(JSON.string(addr, null, 2));
